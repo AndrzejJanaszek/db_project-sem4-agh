4a9f4a869e843672f5d6a5397110ac9f55e1a424
Andrzej Janaszek <janaszek.andrzej.szkola@gmail.com>
2025-04-30 23:29:32 +0200

    feat: Add basic react router navigation, pages and components direction

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 import './App.css';
+
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          {/* <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} /> */}
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
